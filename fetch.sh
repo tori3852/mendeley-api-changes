e69c59cd18ff3848cf87f2afc52d4629eac468be
tr <tr>
2018-03-18 22:07:02 +0000

    Checkout copy of self

--- fetch.sh
+++ fetch.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 echo "Cleaning workspace"
-[[ -d apis/ ]] && rm -rf apis/
-mkdir apis/
+[[ -d mendeley-api-changes/ ]] && rm -rf mendeley-api-changes/
+git clone git@github.com:tori3852/mendeley-api-changes.git
+[[ -d mendeley-api-changes/apis/ ]] && rm -rf mendeley-api-changes/apis/
+mkdir mendeley-api-changes/apis/
 
 echo "Fetching API"
 endpoints=$(curl -s "https://api.mendeley.com/apidocs/apis" | python -c 'import sys, json; print "\n".join(list(map(lambda ep: ep["path"], json.load(sys.stdin)["apis"])));' | sort)
@@ -14,16 +16,16 @@ for endpoint in $endpoints;
     echo "Endpoint: $endpoint"
     endpoint="$(echo $endpoint | sed 's|^/||')"
     file="$(echo $endpoint | sed 's|/|_|g').json"
-    curl -s "https://api.mendeley.com/apidocs/apis/$endpoint" | python -m json.tool > "apis/$file"
+    curl -s "https://api.mendeley.com/apidocs/apis/$endpoint" | python -m json.tool > "mendeley-api-changes/apis/$file"
 done
 
 echo "Recording changes"
 
-git add apis/
-git commit -m "API changes for: $(date)"
+git -C mendeley-api-changes/ add apis/
+git -C mendeley-api-changes/ commit -m "API changes for: $(date)"
 
 echo "Pushing changes"
-git push
+git -C mendeley-api-changes/ push
 
 echo "Done."
 
