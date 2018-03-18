989a2ef61353587ac786733c6e80e18a547b6f7f
tr <tr>
2018-03-18 21:10:24 +0000

    Replace jq with python

--- fetch.sh
+++ fetch.sh
@@ -7,14 +7,14 @@ echo "Cleaning workspace"
 mkdir apis/
 
 echo "Fetching API"
-endpoints=$(curl -s "https://api.mendeley.com/apidocs/apis" | jq -r '.apis[].path' | sort)
+endpoints=$(curl -s "https://api.mendeley.com/apidocs/apis" | python -c 'import sys, json; print "\n".join(list(map(lambda ep: ep["path"], json.load(sys.stdin)["apis"])));' | sort)
 
 for endpoint in $endpoints;
   do
     echo "Endpoint: $endpoint"
     endpoint="$(echo $endpoint | sed 's|^/||')"
     file="$(echo $endpoint | sed 's|/|_|g').json"
-    curl -s "https://api.mendeley.com/apidocs/apis/$endpoint" | jq -S . > "apis/$file"
+    curl -s "https://api.mendeley.com/apidocs/apis/$endpoint" | python -m json.tool > "apis/$file"
 done
 
 echo "Recording changes"
